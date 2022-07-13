18b42f953eea8e6ee68080eaff64204241fd25db
jleht <ggmoox@gmail.com>
2022-07-13 15:07:13 +0300

    changed drag and drop to folder

--- plotting.py
+++ plotting.py
@@ -1,33 +1,39 @@
-from cProfile import label
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sys
 import re
+import os
+
 
 def plot_selected():
-    paths = input("Drop the files you want to plot and press enter\n")
-    paths = paths.replace(" ", "")
-    paths = re.findall(r"'(.*?)'", paths, re.DOTALL)
-    for path in paths:
+    filenames = []
+    if not os.path.exists('./selected_plots'):
+        os.makedirs('./selected_plots')
+    for file in os.listdir('./selected_plots'):
+        if file.endswith('.csv'):
+            filenames.append(file)
+    for name in filenames:
         found = False
-        with open(path) as readfile:
+        with open('./selected_plots/'+name) as readfile:
             for cnt, line in enumerate(readfile):
                 if "wavelength,intensity" in line:
                     row_num = cnt
                     found = True
         if not found:continue
         readfile.close
-        df = pd.read_csv(path, skiprows=row_num)
+        df = pd.read_csv('./selected_plots/'+name, skiprows=row_num)
         plt.rc('xtick',labelsize=12)
         plt.rc('ytick',labelsize=12)
         plt.rc('axes', labelsize=12)
         plt.xlabel('Wavelength')
         plt.ylabel('Intensity')
         plt.ylim((0,60000))
-        plt.plot(df['wavelength'], df['intensity'], label=path.rsplit('/', 1)[-1])
+        plt.plot(df['wavelength'], df['intensity'], label=name.rsplit('/', 1)[-1])
         plt.legend(loc='upper right')
 
     plt.show()
 
+if __name__ == "__main__":
+    plot_selected()
